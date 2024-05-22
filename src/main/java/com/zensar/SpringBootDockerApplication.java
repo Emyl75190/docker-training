1c7f08ea3158644792b8902464a44ef43413ffa2
Emyl75190 <e.george@zensar.com>
2024-05-22 11:48:07 +0530

    Changes to the Application file

--- src/main/java/com/zensar/SpringBootDockerApplication.java
+++ src/main/java/com/zensar/SpringBootDockerApplication.java
@@ -2,12 +2,21 @@ package com.zensar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.zensar.controllers.HelloController;
 
 @SpringBootApplication
-public class SpringBootDockerApplication {
+public class SpringBootDockerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HelloController.class);
 	}
 
 }
